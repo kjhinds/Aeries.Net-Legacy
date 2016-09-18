ed6b9991a22f2d9ee0d567fa7220259e5608da12
kjhinds <kjhinds@gmail.com>
2016-09-17 20:14:25 -0700

    Working version

--- Aeries.Net Web Import/app/popup.js
+++ Aeries.Net Web Import/app/popup.js
@@ -1,83 +1,76 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'kittens';
-
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
-
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
-
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
-
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
+function importScores() {
+    if (document.getElementById('StuName').checked) {
+        var identType = 'StuName';
+    } else if (document.getElementById('StuNum').checked) {
+        var identType = 'StuNum';
+    } else {
+        var identType = 'PermID';
+    };
+    var dataObj = {
+        assignment: document.getElementById('assignment').value,
+        identtype: identType,
+        scores: repackScores(document.getElementById('scores').value, identType),
+        command: "import"
+    };
+    sendToContentScript(dataObj);
 };
 
-// Run our kitten generation script as soon as the document's DOM is ready.
-document.addEventListener('DOMContentLoaded', function () {
-  kittenGenerator.requestKittens();
+function repackScores(scores, identType) {
+    var scoresObject = {};
+    var scoresArray = scores.split("\n");
+    if (identType == 'StuName') {
+        for (i = 0; i < scoresArray.length; i++) {
+            if (scoresArray[i]) {
+                var student = scoresArray[i].split(/\t|,/);
+                var lastName = student[0].trim();
+                var firstName = student[1].trim();
+                var score = student[2].trim();
+                scoresObject[i] = 
+                    {lastName: lastName,
+                        firstName: firstName,
+                        Score: score};
+            };
+        };
+    } else if (identType == 'StuNum') {
+        for (i = 0; i < scoresArray.length; i++) {
+            var student = scoresArray[i].split(/\t|,/);
+            scoresObject[i] =
+                {StuNum: student[0], Score: student[1]};
+        }
+    } else {
+        // for Perm ID
+    };
+    return scoresObject;
+};
+
+function sendToContentScript(message) {
+    chrome.tabs.getSelected(null, function (tab) {
+        chrome.tabs.sendMessage(tab.id, { command: message.command, message: message }, function () {
+        });
+    });
+};
+
+function getRoster() {
+    sendToContentScript({ command: "getRoster" });
+    chrome.tabs.create({
+        url: chrome.extension.getURL('roster.html'),
+        active: false
+    }, function (tab) {
+        // After the tab has been created, open a window to inject the tab
+        chrome.windows.create({
+            tabId: tab.id,
+            type: 'popup',
+            focused: true,
+            width: 560,
+            height: 720
+            // incognito, top, left, ...
+        });
+    });
+}
+
+// When the popup HTML has loaded
+window.addEventListener("load", function (evt) {
+    document.getElementById('importscores').addEventListener('submit', importScores);
+    document.getElementById('close').onclick = function () { window.close() };
+    document.getElementById('getroster').onclick = function () { getRoster() };
 });
---
+++ Aeries.Net Web Import/app/script.js
@@ -0,0 +1,168 @@
+var inputScore = function (StudentNumber, StudentScore, Assignment) {
+    //document.getElementsByClassName("assignments")[0].rows[0].cells[7].children[0].textContent = "100";
+    var foundName = false;
+    var students = document.getElementsByClassName("assignments")[0].rows;
+    for (var row = 0; row < students.length; row++) {
+        for (var sn = 0; sn < students[row].attributes.length; sn++) {
+            if (students[row].attributes[sn].name == "data-sn" &&
+                students[row].attributes[sn].value == StudentNumber) {
+                //console.log("worked!");
+                foundName = true;
+                    var assignments = students[row].cells;
+                    for (var an = 0; an < assignments.length; an++) {
+                        for (var attr = 0; attr < assignments[an].attributes.length; attr++) {
+                            if (assignments[an].attributes[attr].name == "data-an" &&
+                                assignments[an].attributes[attr].value == Assignment) {
+                                //console.log(assignments[an]);
+                                //console.log(StudentScore);
+
+                                assignments[an].children[0].textContent = StudentScore;
+                                assignments[an].dispatchEvent(new MouseEvent("click", {
+                                    "view": window,
+                                    "bubbles": true,
+                                    "cancelable": false
+                                }));
+                                assignments[an].dispatchEvent(new Event("keypress", {which: 13}));
+                            }
+                        }
+                    }
+            }
+        }
+    }
+    if (!foundName) {
+        var notFound = "Could not find student numbers:\n";
+        notFound += StudentNumber;
+        notFound += ", Score: ";
+        notFound += StudentScore;
+        notFound += "\n";
+        return notFound;
+    }
+    else {
+        return "";
+    }
+    //for (var ID in ScoresTable) {
+    //    if(self["AN"+ID+"_"+Assignment]) {
+    //        self["AN"+ID+"_"+Assignment].value = ScoresTable[ID];
+    //    } else {
+    //        notFound += ID;
+    //        notFound += " -> ";
+    //        notFound += ScoresTable[ID];
+    //        notFound += "\n";
+    //    }
+    //}
+    //if (notFound.length > 35) {window.alert(notFound)};
+};
+
+var createScoresTable = function (scores, identtype) {
+    //console.log(scores);
+    //console.log(identtype);
+    if(identtype == 'StuNum') {
+        return scores;
+    } else if(identtype == 'StuName') {
+        var notFound = "Could not find:\n";
+        var roster = getRoster();
+        var scoresTable = [];
+        for ( var i in scores) {
+            var lastName = scores[i].lastName;
+            var firstName = scores[i].firstName;
+            var foundName = false;
+            for ( j in roster) {
+                var rosterName = roster[j].StuName.split(", ");
+                if ( lastName == rosterName[0] && firstName == rosterName[1] ||
+                     firstName == rosterName[0] && lastName == rosterName[1]) {
+                    scoresTable.push({ StuNum: roster[j].StuNum, Score: scores[i].Score });
+                    foundName = true;
+                }
+            }
+            if (!foundName) {
+                notFound += lastName + ", " + firstName;
+                notFound += ", Score: ";
+                notFound += scores[i].Score;
+                notFound += "\n";
+            }
+        }
+        if (notFound.length > 18) { window.alert(notFound) };
+        //console.log(scoresTable);
+        return scoresTable;
+    } else {
+        return scores;  // for Permanent ID, not implemented
+    };
+};
+
+var getRoster = function(){
+    var StudentTable = {};
+    var students = document.getElementsByClassName("student-name-link");
+    for (var i = 0; i < students.length; i++) {
+        StudentTable[i] = {
+            StuNum: getStudentNumber(students[i].textContent),
+            StuName: students[i].textContent
+        }
+    }
+    return StudentTable;
+};
+
+var repackRoster = function(){
+    var StudentList = "";
+    var students = document.getElementsByClassName("student-name-link");
+    for (var i = 0; i < students.length; i++) {
+        StudentList += getStudentNumber(students[i].textContent);
+        StudentList += "\t";
+        StudentList += students[i].textContent;
+        StudentList += "\n";
+    }
+
+    return StudentList;
+};
+
+//var getStudentName = function (student) {
+//    var tempName = student.textContent;
+//    //var tempName = document.getElementById("Row"+rowNumber).children[1].children[0].innerHTML.split("<")[0].replace(/&nbsp;/g," ");
+//    var name = tempName.split(", ");
+//    var lastName = name[0].split(" ");
+//    var firstName = name[1].split(" ");
+//    return lastName[0] + ", " + firstName[0];
+//}
+
+var getStudentNumber = function (studentName) {
+    var students = document.getElementsByClassName("students")[0].rows;
+    for (var row = 0; row < students.length; row++) {
+        if (students[row].cells[1].children[0].children[0].children[0].textContent == studentName)
+        {
+            return students[row].attributes[1].value;
+        }
+    }
+}
+
+var sendToContentScript = function(message, command){
+    window.postMessage({ text: message, command: command}, '*');
+};
+
+var handleImport = function (scores, identType, assignment) {
+    var scoresTable = createScoresTable(scores, identType);
+    var notFoundList = "";
+    for (var student in scoresTable) {
+        notFoundList += inputScore(scoresTable[student].StuNum, scoresTable[student].Score, assignment);
+    }
+    if (notFoundList != "")
+    {
+        window.alert(notFoundList)
+    }
+}
+
+window.addEventListener('message', function(event) {
+    if (event.origin != "https://teacherportal.abcusd.us") {
+        return false;
+    } else {
+        switch(event.data.text.command) {
+            case "import":
+                handleImport(event.data.text.scores, event.data.text.identtype, event.data.text.assignment);
+                break;
+            case "getRoster":
+                var roster = repackRoster();
+                sendToContentScript(roster, "openPopup");
+                break;   
+            default:
+                return false;
+        }
+    };
+});
